dea60fc224b96c5255e4eeb24d9b8afdaeb32282
ZhukMax <zhukmax@ya.ru>
2016-09-30 20:23:31 +0300

    fixes

--- src/Phoxie.php
+++ src/Phoxie.php
@@ -1,5 +1,12 @@
 <?php
-
+/*
+ * This file is part of the phoxie package.
+ *
+ * (c) Zhuk Max <zhukmax@ya.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace Zhukmax\Phoxie;
 
 use Zhukmax\Phoxie\Exceptions\PhoxieException;
