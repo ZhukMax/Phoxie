43e23da22b9db9cc063472672bce83682aa30b25
ZhukMax <zhukmax@ya.ru>
2016-09-30 03:09:57 +0300

    start

--- src/SkeletonClass.php
+++ src/SkeletonClass.php
@@ -1,1 +1,25 @@
 <?php
+namespace Zhukmax\Phoxie;
+
+class SkeletonClass
+{
+
+    /**
+     * Создаём новый экземпляр шаблонного приложения
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Дружелюбное приветствие
+     *
+     * @param string $phrase Возвращаемая фраза
+     *
+     * @return string Возвращает переданную фразу
+     */
+    public function echoPhrase($phrase)
+    {
+        return $phrase. '<br>Тест прошел успешно!';
+    }
+}
