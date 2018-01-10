42192a143cbe1d1f95c24084df328691afad502f
Max Zhuk <zhukmax@ya.ru>
2018-01-11 02:31:14 +0300

    Update index.js

--- admin/index.js
+++ admin/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import Feap from 'feap';
 
 let navigationItems = {
-    "users": "Пользователи",
-    "media": "Медиа-файлы"
+    "users": "Users",
+    "media": "Media files"
 };
 
 const AdminFront = (
